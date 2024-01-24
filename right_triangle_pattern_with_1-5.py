b42ee23bca20cf1cdafa07394a98b0475d5fe26f
unknown <centinaraphael0157@gmail.com>
2024-01-24 23:52:25 +0800

    Can continue thhis code but it will only be less efficient than print

--- right_triangle_pattern_with_1-5.py
+++ right_triangle_pattern_with_1-5.py
@@ -4,5 +4,10 @@
 #3 3 3
 #4 4 4 4 
 #5 5 5 5 5
-print('Here is the required pattern')
-print('1\n2 2\n3 3 3\n4 4 4 4\n5 5 5 5 5')
+for i in range (4):
+    if i == 1:
+        print(i)
+    if i == 2:
+        for _ in range (1):
+            contant_for_second_line = 2
+            print(i,str(contant_for_second_line))
