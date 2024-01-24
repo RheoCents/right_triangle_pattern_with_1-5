b7e2c71ca2211c4dd20740934a1da35f27f35d9f
RheoCents <150512072+RheoCents@users.noreply.github.com>
2024-01-25 00:38:00 +0800

    added print initilization

--- right_triangle_pattern_with_1-5.py
+++ right_triangle_pattern_with_1-5.py
@@ -4,6 +4,7 @@
 #3 3 3
 #4 4 4 4 
 #5 5 5 5 5
+print('For this, we will print a figure of right triangle using the print function and numbers 1-5')
 for number_value in range(6):
     print('\n')
     for _ in range(number_value):
